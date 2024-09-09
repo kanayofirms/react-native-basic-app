2759c61e062def055084ff83944ba2b4710b93b8
kanayofirms <kanyeze@gmail.com>
2024-09-09 19:40:44 +0100

    Update ai app

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">AI-APP!!!</Text>
+            <StatusBar style="auto" />
+            <Link href='/profile' style={{ color: 'blue'}}> Go To Profile</Link>
+        </View>
+    )
+}
+
