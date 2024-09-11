e11dd50fb2fe347c36f9dc3413ff2d54a02a8ae4
kanayofirms <kanyeze@gmail.com>
2024-09-11 06:37:37 +0100

    Update auth and tabs files

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,59 +1,21 @@
 import { View, Text } from 'react-native'
-import { Tabs, Redirect } from 'expo-router'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-import { icons } from '../../constants';
-
-const TabIcon = ({ icon, color, name, focused }) => {
-  return (
-    <View className='items-center justify-center gap-2'>
-      <Image
-        Source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
-      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs `} style={{ color: color }}>
-        {name}
-      </Text>
-    </View>
-  )
-}
-const TabsLayout = () => {
+const AuthLayout = () => {
   return (
     <>
-      <Tabs screenOptions={{
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#FFA001',
-        tabBarInactiveTintColor: '#CDCDE0',
-        tabBarStyle: {
-          backgroundColor: '#161622',
-          borderTopWidth: 1,
-          borderTopColor: '#232533',
-          height: 84,
-        }
-      }}
-      >
-        <Tabs.Screen name="home" options={{
-          title: 'Home',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <TabIcon icon={icons.home}  color={color} name="Home" focused={focused}/>
-          )
+      <Stack>
+        <Stack.Screen name='sign-in' options={{
+          headerShown: false
         }} />
-         <Tabs.Screen name="bookmark" options={{
-          title: 'Bookmark',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <TabIcon icon={icons.bookmark}  color={color} name="Bookmark" focused={focused}/>
-          )
+        <Stack.Screen name='sign-up' options={{
+          headerShown: false
         }} />
-         <Tabs.Screen name="profile" options={{
-          title: 'Profile',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <TabIcon icon={icons.profile}  color={color} name="Profile" focused={focused}/>
-          )
-        }} />
-        
-      </Tabs>
+      </Stack>
+      <StatusBar backgroundColor='#161622' style='light' />
     </>
   )
 }
 
-export default TabsLayout
+export default AuthLayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Image, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
@@ -10,7 +10,7 @@ export default function App() {
     return (
         <SafeAreaView className='bg-primary h-full'>
             <ScrollView contentContainerStyle={{ height: '100%' }}>
-                <View className="w-full justify-center items-center h-full px-4">
+                <View className="w-full justify-center items-center min-h-[85vh] px-4">
                     <Image source={images.logo} className="w-0[130px] h-[84px]" resizeMode="contain" />
                     <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain" />
                     
@@ -27,7 +27,7 @@ export default function App() {
                     </Text>
                     <CustomButton
                         title="Continue with Email"
-                        handlePress={() => { }}
+                        handlePress={() => router.push('/sign-in')}
                         containerStyles="w-full mt-7"
                     />
                 </View>
