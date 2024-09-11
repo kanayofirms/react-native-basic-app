7cf762f1edaab4cef575657ac68e935fe52f995a
kanayofirms <kanyeze@gmail.com>
2024-09-11 20:58:08 +0100

    Refactor custom button for feasibility

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,12 +3,19 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
     password: ''
   })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+  
+}
 
   return (
     <SafeAreaView className="bg-primary h-full">  
@@ -33,6 +40,19 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
           />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
