e63d5255e99175f66bb3383cf76c5a5339e0fb8e
kanayofirms <kanyeze@gmail.com>
2024-09-11 22:54:02 +0100

    Refactor code for feasibility

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
+
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -20,7 +22,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">  
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[83vh] px-4 my-6">
           <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]' />
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
@@ -52,6 +54,7 @@ const SignIn = () => {
             <Text className="text-lg text-gray-100 font-pregular">
               Don't have an account?
             </Text>
+            <Link href={sign-up} className='text-lg font-semibold text-secondary'>Sign Up</Link>
           </View>
         </View>
       </ScrollView>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { SplashScreen, Slot, Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
 
@@ -29,7 +29,10 @@ const RootLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false}} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />
     </Stack>
   ) 
 }
