ecac9ae76bd50e7f39ba4a8f49fe23210d12e701
kanayofirms <kanyeze@gmail.com>
2024-09-17 16:40:45 +0100

    [Feature]([SignIn Screen]): Implement sign-in screen UI with email and password fields

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,29 +1,30 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants';
-import FormField from '../../components/FormField';
-import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
-
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
-  
-}
+  const submit = () => {};
 
   return (
-    <SafeAreaView className="bg-primary h-full">  
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="w-full justify-center min-h-[83vh] px-4 my-6">
-          <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]' />
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
           </Text>
@@ -54,12 +55,17 @@ const SignIn = () => {
             <Text className="text-lg text-gray-100 font-pregular">
               Don't have an account?
             </Text>
-            <Link href="/sign-up" className='text-lg font-semibold text-secondary'>Sign Up</Link>
+            <Link
+              href="/sign-up"
+              className="text-lg font-semibold text-secondary"
+            >
+              Sign Up
+            </Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
