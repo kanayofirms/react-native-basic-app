2b57398fb21ebc5c459f90016df29a87a000fd8e
kanayofirms <kanyeze@gmail.com>
2024-09-20 12:46:38 +0100

    fix: Update keyExtractor to handle null $id values

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,11 +1,25 @@
 import { View, Text, FlatList } from "react-native";
 import React from "react";
 
+// const Trending = ({ posts }) => {
+//   return (
+//     <FlatList
+//       data={posts}
+//       keyExtractor={(item) => item.$id}
+//       renderItem={({ item }) => (
+//         <Text className="text-3xl text-white">{item.id}</Text>
+//       )}
+//       horizontal
+//     />
+//   );
+// };
+
 const Trending = ({ posts }) => {
+  console.log("Posts:", posts);
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.$id}
+      keyExtractor={(item, index) => item?.$id ?? index.toString() ?? ""} // Convert $id to string && add null check
       renderItem={({ item }) => (
         <Text className="text-3xl text-white">{item.id}</Text>
       )}
